6cf763f0a06ed58f210826e639ae6d5b83d6e831
Taisuke Fukuno <fukuno@jig.jp>
2021-08-22 08:02:43 +0900

    add nearby.html, searchNearBy.js

--- searchNearBy.js
+++ searchNearBy.js
@@ -1,67 +1,29 @@
-import { sleep } from "https://js.sabae.cc/sleep.js";
 import { CSV } from "https://js.sabae.cc/CSV.js";
-
-/*
-const cls = async (color) => {
-	document.body.style.backgroundColor = color || "black";
-	await sleep(100);
-};
-*/
-const waitImageLoad = async (img) => {
-	return new Promise((resolve) => {
-		img.onload = () => {
-			resolve(img);
-		};
-	});
-};
+import { getDistance } from "https://js.sabae.cc/getDistance.js";
+import { Geo3x3 } from "https://geo3x3.com/Geo3x3.js";
 
 let imglist = null;
-const bg = async (no, nowait) => {
+
+export const searchNearBy = async (lat, lng, radiuskm) => {
 	if (!imglist) {
-		imglist = CSV.toJSON(await CSV.fetch("https://code4fukui.github.io/find47/find47images.csv"));
+		imglist = CSV.toJSON(await CSV.fetch("https://code4fukui.github.io/find47/find47images_c.csv"));
 		//console.log(imglist);
 	}
-	const data = imglist.find(i => i.id == no);
-	if (!data) {
-		return;
-	}
-	const img = new Image();
-	img.src = data.url_image;
-	await waitImageLoad(img);
-	//document.body.style.backgroundImage = `url('${url}'), url("./man.png")`;
-	document.body.style.backgroundColor = "black";
-	document.body.style.backgroundImage = `url('${data.url_image}')`;
-	document.body.style.backgroundRepeat = "no-repeat";
-	document.body.style.backgroundSize = "100% auto";
+	return imglist.filter(i => {
+		const ll = Geo3x3.decode(i.Geo3x3);
+		const d = getDistance(lat, lng, ll.lat, ll.lng);
+		return d < radiuskm;
+	}).map(i => {
+		const ll = Geo3x3.decode(i.Geo3x3);
+		const distance = getDistance(lat, lng, ll.lat, ll.lng);
+		// 1,NzJVV,https://find47.jp/ja/i/NzJVV,https://find47.jp/en/i/NzJVV,https://find47.jp/ja/i/NzJVV/image_file,https://find47.jp/ja/i/NzJVV/image_file?type=thumb,能取岬,Cape Notoro Hokkaido,1,北海道,Hokkaido,小西 正敏,Masatoshi Konishi,,,https://creativecommons.org/licenses/by/4.0/,,1980,5440,E92726315617716464,,,,,,,,,
 
-	const get = (id) => {
-		const c = document.getElementById(id);
-		if (c) {
-			return c;
+		return {
+			title: i.title,
+			Geo3x3: i.Geo3x3,
+			distance,
+			url: `https://find47.jp/ja/i/${i.code}`,
+			url_thumb: `https://find47.jp/ja/i/${i.code}/image_file?type=thumb`,
 		}
-		const c2 = document.createElement("a");
-		c2.id = id;
-		document.body.appendChild(c2);
-		return c2;
-	};
-	const div = get("find47_bg_credit");
-	div.style.position = "absolute";
-	div.style.right = ".2em";
-	div.style.bottom = ".2em";
-	div.href = data.url;
-	div.style.textDecoration = "none";
-	div.textContent = data.title + " © " + data.author + " クリエイティブ・コモンズ・ライセンス（表示4.0 国際）";
-	div.style.color = "white";
-	div.style.fontSize = "70%";
-	if (!nowait) {
-		await sleep(1000);
-	}
-	/*
-	document.body.style.backgroundImage = `url("./man.png")`;
-	document.body.style.backgroundImage = `url("./man.png")`;
-	document.body.style.backgroundSize = "auto 50%";
-	document.body.style.backgroundPosition = "20% 100%";
-	*/
+	});
 };
-
-export { bg };
